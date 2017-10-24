ef2651701107d18825ebae122e5803417f59ac0c
Nicolas Marsan <nicolasmarsan33@gmail.com>
2017-10-24 10:26:36 +0200

    get_next_line.c modification

--- get_next_line.c
+++ get_next_line.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
+/*
+** We don't know the final size of the line so we're unable to predict
+** what Data type we'll need ===> "size_t"
+*/
+
 char			*my_realloc(char *line, size_t size_reallocated_line)
 {
   char			*reallocated_line;
@@ -19,6 +24,12 @@ char			*my_realloc(char *line, size_t size_reallocated_line)
   free(line);
   return (reallocated_line);
 }
+
+/*
+** line will be reallocated only when its size (--) has reached 0, that's is to say
+** we can modify the pointer (++) to catch & check each char
+** So, char will be = to the line 1st char, the *line
+*/
 
 static char		get_char_by_char(const int fd)
 {
@@ -40,6 +51,15 @@ static char		get_char_by_char(const int fd)
   --ret_read;
   return (character);
 }
+
+/*
+** If the 1st char is NUL, return NULL
+** We'll catch the file content and check char-by-char the LF or NUL.
+** If the READ_SIZE is smaller than the line size, realloc using the index created in the loop
+** 
+** (index % (READ_SIZE + 1) == 0), remember that the remainder of the modulo will be 0 if index
+** is a multiple of (READ_SIZE + 1) 
+*/
 
 char			*get_next_line(const int fd)
 {
